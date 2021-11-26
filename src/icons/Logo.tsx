e8aa4be0de822709acd36e662be76b1e27a75071
David Trafford <d.trafford@outlook.com>
2021-11-26 13:30:40 -0500

    remvoe logo props

--- src/icons/Logo.tsx
+++ src/icons/Logo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const Logo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+export const Logo: React.FC<React.SVGProps<SVGSVGElement>> = () => (
   <svg
     version="1.0"
     xmlns="http://www.w3.org/2000/svg"
